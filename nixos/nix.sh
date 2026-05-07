69d6c44e89df9202fdc96ed45f4054638ffae8ee
tau-zeta <taufiqhaikal1808@gmail.com>
2026-05-07 13:36:04 +0800

    Delete nixos/nix.sh

--- nixos/nix.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -p vim git --run "bash ./install.sh"
-
-# git clone https://github.com/icedcoffeeee/nixos
-# cd nixos
-# vim flake.nix
-# sudo nixos-rebuild build --flake . --impure
