797ee69b44c2412f4a698e3ada3220dfbd11129a
tau-zeta <taufiqhaikal1808@gmail.com>
2026-05-07 13:35:49 +0800

    Pushed from local

---
+++ nixos/nix.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -p vim git --run "bash ./install.sh"
+
+# git clone https://github.com/icedcoffeeee/nixos
+# cd nixos
+# vim flake.nix
+# sudo nixos-rebuild build --flake . --impure
---
+++ nixos/packages.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, lib, ... }:
+
+{
+	programs.steam.enable = true;
+	programs.steam.gamescopeSession.enable = true;
+	programs.gamemode.enable = true;
+
+	environment.systemPackages = with pkgs; [
+		# core utils
+		wget 
+		curl 
+		wl-clipboard 
+		zip
+		unzip 
+		p7zip 
+		libarchive 
+		flatpak 
+		htop 
+		fzf 
+		ripgrep 
+		fd 
+		auto-cpufreq 
+		killall 
+		brightnessctl
+
+		# dev tools
+		git 
+		gcc 
+		gnumake 
+		cmake 
+		gfortran 
+		python3 
+		uv 
+		rustup 
+		typst 
+		starship
+
+		# apps
+		kitty
+		zathura 
+		libreoffice 
+		spotify
+		protonup-qt
+		
+		# misc
+		bibata-cursors
+	];
+}
